a7305cfe3fe98494e9dce002ed99689d7d1d45e1
Ilya Loginov <ilya2707004@yandex.ru>
2019-09-24 16:38:28 +0500

    Реализовал универсальный профиль( Отображает Логин, Квалификацию, ФИО, Емеил, Номер)
    Добавил возможность менять личную инфу(
    ФИО, Емеил, Номер, Пароль)
    
    Новый столбец в БД для проверка первого входа в систему
    
    Обновил пермишены

--- CRM/views/auth.py
+++ CRM/views/auth.py
@@ -5,6 +5,7 @@ from django.views.generic import (
     RedirectView, TemplateView, UpdateView,
 )
 from rules.contrib.views import PermissionRequiredMixin
+from CRM.forms import ProfileAdminForm
 
 
 class CrmLoginRedirectView(RedirectView):
@@ -14,11 +15,39 @@ class CrmLoginRedirectView(RedirectView):
             return reverse('accounts:login')
         elif self.request.user.is_superuser:
             return reverse('admin:index')
-        elif self.request.user.is_admin:
-            return reverse('accounts:login')
-        elif self.request.user.is_manager:
-            return reverse('accounts:login')
-        elif self.request.user.is_worker:
-            return reverse('accounts:login')
+        elif not self.request.user.is_first_login:
+            self.request.user.get_update_first_user_login()
+            return reverse('accounts:password-change')
+        elif self.request.user.is_admin or self.request.user.is_manager or self.request.user.is_worker:
+                return reverse('accounts:profile')
         else:
             return reverse('accounts:login')
+
+
+class PasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+    success_url = reverse_lazy('accounts:profile')
+    template_name = 'CRM/auth/password-change.html'
+
+
+class ProfileView(PermissionRequiredMixin, UpdateView):
+    template_name = 'CRM/auth/profile.html'
+    success_url = reverse_lazy('accounts:profile')
+    permission_required = 'profile'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_form_class(self):
+        if self.request.user.is_admin or self.request.user.is_manager or self.request.user.is_worker:
+            return ProfileAdminForm
+        else:
+            return reverse('accounts:login')
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update(instance={
+            'user': self.object,
+            'detail':
+                self.object
+        })
+        return kwargs
